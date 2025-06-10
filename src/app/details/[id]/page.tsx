8068cdf15544a2c568c50fc0fd67354def532051
xingzifan <xingzifan@optfuture.cn>
2025-06-10 18:37:52 +0800

    feat: 暂存

--- src/app/details/[id]/page.tsx
+++ src/app/details/[id]/page.tsx
@@ -31,18 +31,18 @@ export async function generateMetadata() {
 
 export default async function Details({ params }: any) {
   const resolvedParams = await params;
-  // const apiUrl = `${process.env.TMDB_API_BASE_URL}/3/movie/950387?api_key=${process.env.TMDB_API_KEY}`;
-  // console.log(44444, apiUrl);
+  const apiUrl = `${process.env.TMDB_API_BASE_URL}/3/movie/950387?api_key=${process.env.TMDB_API_KEY}`;
+  console.log(44444, apiUrl);
 
-  // const response = await fetch(apiUrl, {
-  //   next: { revalidate: 60 * 60 * 24 }, // 缓存时间：控制数据缓存和重新验证的关键
-  //   // cache: 'force-cache' // 默认行为，除非指定 revalidate: 0 或 no-store
-  //   // cache: 'no-store' // 完全禁用缓存，每次请求都重新获取
-  // });
+  const response = await fetch(apiUrl, {
+    next: { revalidate: 60 * 60 * 24 }, // 缓存时间：控制数据缓存和重新验证的关键
+    // cache: 'force-cache' // 默认行为，除非指定 revalidate: 0 或 no-store
+    // cache: 'no-store' // 完全禁用缓存，每次请求都重新获取
+  });
 
-  // const res: any = await response.json();
+  const res: any = await response.json();
 
-  // console.log(5555, res);
+  console.log(5555, res);
 
   // const apiUrl = `${process.env.TMDB_API_BASE_URL}/3/discover/movie?api_key=${
   //   process.env.TMDB_API_KEY
